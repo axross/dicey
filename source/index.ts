8e2788b3d6cd2422c01053c311feafdf45fc3d24
axross <me@axross.io>
2016-08-22 18:09:14 +0900

    Add an index file

---
+++ source/index.ts
@@ -0,0 +1,5 @@
+export { default as getRandomValue } from './getRandomValue';
+export { default as getRandomDouble } from './getRandomDouble';
+export { default as getRandomInteger } from './getRandomInteger';
+export { default as getRandomString } from './getRandomString';
+export { default as getRandomDate } from './getRandomDate';
