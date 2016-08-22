54be70f34c373ff69a6a02d53cd293db9523e90a
axross <me@axross.io>
2016-08-22 18:07:11 +0900

    Refactor the getRandomDouble() and write the Test

---
+++ source/__tests__/getRandomDouble.test.ts
@@ -0,0 +1,65 @@
+import test from 'ava';
+import getRandomDouble from '../getRandomDouble';
+
+const TRIALS = 1000000;
+const AMPLITUDE_MAGNIFICATION = 0.005;
+
+test('(minimum, maximum) returns a Number at random with arguments. #1', t => {
+  const MINIMUM = -50;
+  const MAXIMUM = 50;
+  const MEDIAN = (MAXIMUM - MINIMUM) / 2 + MINIMUM;
+  const AMPLITUDE = (MAXIMUM - MINIMUM) / 2 * AMPLITUDE_MAGNIFICATION;
+  let total = 0;
+
+  for (let i = 0; i < TRIALS; ++i) {
+    const result = getRandomDouble(MINIMUM, MAXIMUM);
+
+    if (result < MINIMUM || result > MAXIMUM) {
+      t.fail(`${result} is an invalid value for between ${MINIMUM} and ${MAXIMUM}`);
+    }
+
+    total += result;
+  }
+
+  t.true(total >= TRIALS * (MEDIAN - AMPLITUDE) && total <= TRIALS * (MEDIAN + AMPLITUDE));
+});
+
+test('(minimum, maximum) returns a Number at random with arguments. #2', t => {
+  const MINIMUM = 0;
+  const MAXIMUM = 100;
+  const MEDIAN = (MAXIMUM - MINIMUM) / 2 + MINIMUM;
+  const AMPLITUDE = (MAXIMUM - MINIMUM) / 2 * AMPLITUDE_MAGNIFICATION;
+  let total = 0;
+
+  for (let i = 0; i < TRIALS; ++i) {
+    const result = getRandomDouble(MINIMUM, MAXIMUM);
+
+    if (result < MINIMUM || result > MAXIMUM) {
+      t.fail(`${result} is an invalid value for between ${MINIMUM} and ${MAXIMUM}`);
+    }
+
+    total += result;
+  }
+
+  t.true(total >= TRIALS * (MEDIAN - AMPLITUDE) && total <= TRIALS * (MEDIAN + AMPLITUDE));
+});
+
+test('(minimum, maximum) returns a Number at random with arguments. #3', t => {
+  const MINIMUM = -100;
+  const MAXIMUM = -50;
+  const MEDIAN = (MAXIMUM - MINIMUM) / 2 + MINIMUM;
+  const AMPLITUDE = (MAXIMUM - MINIMUM) / 2 * AMPLITUDE_MAGNIFICATION;
+  let total = 0;
+
+  for (let i = 0; i < TRIALS; ++i) {
+    const result = getRandomDouble(MINIMUM, MAXIMUM);
+
+    if (result < MINIMUM || result > MAXIMUM) {
+      t.fail(`${result} is an invalid value for between ${MINIMUM} and ${MAXIMUM}`);
+    }
+
+    total += result;
+  }
+
+  t.true(total >= TRIALS * (MEDIAN - AMPLITUDE) && total <= TRIALS * (MEDIAN + AMPLITUDE));
+});
--- source/getRandomDouble.ts
+++ source/getRandomDouble.ts
@@ -1,13 +1,20 @@
 import * as assert from 'assert';
 
-const getRandomDouble = (minimum: number = -100, maximum: number = 100): number => {
+const getRandomDouble = (
+  minimum: number = Number.MIN_SAFE_INTEGER,
+  maximum: number = Number.MAX_SAFE_INTEGER,
+): number => {
   assert(
     typeof minimum === 'number' && Number.isFinite(minimum),
-    '`minimum` must be an Finate Number.'
+    'minimum must be an Finate Number.'
   );
   assert(
     typeof maximum === 'number' && Number.isFinite(maximum),
-    '`maximum` must be an Finate Number.'
+    'maximum must be an Finate Number.'
+  );
+  assert(
+    minimum < maximum,
+    'maximum must be greater than minimum.'
   );
 
   return Math.random() * (maximum - minimum) + minimum;
