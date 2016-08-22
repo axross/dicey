708a61e6acd84a819f45488bc1835b6e4070b846
axross <me@axross.io>
2016-08-22 18:05:40 +0900

    Delete getRandomBoolean()

--- source/getRandomBoolean.ts
+++
@@ -1,7 +0,0 @@
-import getRandomInteger from './getRandomInteger';
-
-const VALUES = [true, false];
-
-const getRandomBoolean = () => VALUES[getRandomInteger(0, 1)];
-
-export default getRandomBoolean;
